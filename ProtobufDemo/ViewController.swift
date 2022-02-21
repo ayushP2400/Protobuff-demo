ea4538975353e64801b42c3ff4be6451c5136047
Ayush Pathak <ayush.pathak021@outlook.com>
2022-02-21 18:27:51 +0530

    structured client class

--- ProtobufDemo/ViewController.swift
+++ ProtobufDemo/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import SwiftProtobuf
 
 class ViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         var helloRequest = Helloworld_HelloRequest.init()
@@ -18,46 +18,4 @@ class ViewController: UIViewController {
         GRPCManager.shared.testSayHi(with: helloRequest)
     }
     
-    
-}
-
-import Foundation
-import GRPC
-import SwiftProtobuf
-import NIO
-import NIOSSL
-
-class GRPCManager {
-    
-    var connection: ClientConnection?
-    static let shared = GRPCManager()
-    
-    private init() {
-        let conf = ClientConnection.Configuration.default(target: .hostAndPort("54.67.87.25", 50051), eventLoopGroup: MultiThreadedEventLoopGroup(numberOfThreads: 1))
-        connection = ClientConnection.init(configuration: conf)
-    }
-    func testSayHello(with request: Helloworld_HelloRequest) {
-        guard let connection = connection else { return }
-        let client = Helloworld_GreeterClient(channel: connection)
-        _ = client.sayHello(request).response.always { result in
-            switch result {
-            case let .success(helloResponse):
-                print(helloResponse.message)
-            case let .failure(error):
-                print(error)
-            }
-        }
-    }
-    func testSayHi(with request: Helloworld_HelloRequest) {
-        guard let connection = connection else { return }
-        let client = Helloworld_GreeterClient(channel: connection)
-        _ = client.sayHi(request).response.always { result in
-            switch result {
-            case let .success(helloResponse):
-                print(helloResponse.message)
-            case let .failure(error):
-                print(error)
-            }
-        }
-    }
 }
